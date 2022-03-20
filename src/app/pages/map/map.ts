6d18c0ffea96198c693dac91308bb442cb35f517
Patrick McDonald <764290+WhatsThatItsPat@users.noreply.github.com>
2022-03-20 10:16:33 -0400

    Comment out map and manifest

--- src/app/pages/map/map.ts
+++ src/app/pages/map/map.ts
@@ -1,86 +1,99 @@
-import { Component, ElementRef, Inject, ViewChild, AfterViewInit } from '@angular/core';
-import { ConferenceData } from '../../providers/conference-data';
-import { Platform } from '@ionic/angular';
-import { DOCUMENT} from '@angular/common';
+import {
+  Component,
+  // ElementRef,
+  // Inject,
+  // ViewChild,
+  // AfterViewInit
+} from '@angular/core';
+// import { ConferenceData } from '../../providers/conference-data';
+// import { Platform } from '@ionic/angular';
+// import { DOCUMENT} from '@angular/common';
 
-import { darkStyle } from './map-dark-style';
+// import { darkStyle } from './map-dark-style';
 
 @Component({
   selector: 'page-map',
   templateUrl: 'map.html',
   styleUrls: ['./map.scss']
 })
-export class MapPage implements AfterViewInit {
-  @ViewChild('mapCanvas', { static: true }) mapElement: ElementRef;
+export class MapPage {
+  // @ViewChild('mapCanvas', { static: true }) mapElement: ElementRef;
 
   constructor(
-    @Inject(DOCUMENT) private doc: Document,
-    public confData: ConferenceData,
-    public platform: Platform) {}
+    // @Inject(DOCUMENT) private doc: Document,
+    // public confData: ConferenceData,
+    // public platform: Platform
+  ) {}
 
-  async ngAfterViewInit() {
-    const appEl = this.doc.querySelector('ion-app');
-    let isDark = false;
-    let style = [];
-    if (appEl.classList.contains('dark-theme')) {
-      style = darkStyle;
-    }
+  // async ngAfterViewInit() {
+  //   const appEl = this.doc.querySelector('ion-app');
+  //   let isDark = false;
+  //   let style = [];
+  //   if (appEl.classList.contains('dark-theme')) {
+  //     style = darkStyle;
+  //   }
 
-    const googleMaps = await getGoogleMaps(
-      'YOUR_API_KEY_HERE'
-    );
+  //   const googleMaps = await getGoogleMaps(
+  //     'YOUR_API_KEY_HERE'
+  //   );
 
-    let map;
+  //   let map;
 
-    this.confData.getMap().subscribe((mapData: any) => {
-      const mapEle = this.mapElement.nativeElement;
+  //   this.confData.getMap().subscribe((mapData: any) => {
+  //     const mapEle = this.mapElement.nativeElement;
 
-      map = new googleMaps.Map(mapEle, {
-        center: mapData.find((d: any) => d.center),
-        zoom: 16,
-        styles: style
-      });
+  //     map = new googleMaps.Map(mapEle, {
+  //       center: mapData.find((d: any) => d.center),
+  //       zoom: 16,
+  //       styles: style
+  //     });
 
-      mapData.forEach((markerData: any) => {
-        const infoWindow = new googleMaps.InfoWindow({
-          content: `<h5>${markerData.name}</h5>`
-        });
+  //     mapData.forEach((markerData: any) => {
+  //       const infoWindow = new googleMaps.InfoWindow({
+  //         content: `<h5>${markerData.name}</h5>`
+  //       });
 
-        const marker = new googleMaps.Marker({
-          position: markerData,
-          map,
-          title: markerData.name
-        });
+  //       const marker = new googleMaps.Marker({
+  //         position: markerData,
+  //         map,
+  //         title: markerData.name
+  //       });
 
-        marker.addListener('click', () => {
-          infoWindow.open(map, marker);
-        });
-      });
+  //       marker.addListener('click', () => {
+  //         infoWindow.open(map, marker);
+  //       });
+  //     });
 
-      googleMaps.event.addListenerOnce(map, 'idle', () => {
-        mapEle.classList.add('show-map');
-      });
-    });
+  //     googleMaps.event.addListenerOnce(map, 'idle', () => {
+  //       mapEle.classList.add('show-map');
+  //     });
+  //   });
 
-    const observer = new MutationObserver((mutations) => {
-      mutations.forEach((mutation) => {
-        if (mutation.attributeName === 'class') {
-          const el = mutation.target as HTMLElement;
-          isDark = el.classList.contains('dark-theme');
-          if (map && isDark) {
-            map.setOptions({styles: darkStyle});
-          } else if (map) {
-            map.setOptions({styles: []});
-          }
-        }
-      });
-    });
-    observer.observe(appEl, {
-      attributes: true
-    });
-  }
+  //   const observer = new MutationObserver((mutations) => {
+  //     mutations.forEach((mutation) => {
+  //       if (mutation.attributeName === 'class') {
+  //         const el = mutation.target as HTMLElement;
+  //         isDark = el.classList.contains('dark-theme');
+  //         if (map && isDark) {
+  //           map.setOptions({styles: darkStyle});
+  //         } else if (map) {
+  //           map.setOptions({styles: []});
+  //         }
+  //       }
+  //     });
+  //   });
+  //   observer.observe(appEl, {
+  //     attributes: true
+  //   });
+  // }
 }
 
+
+/**
+ * Interesting...declaring a function outside of the component export.
+ * Can't use `this`. I guess this is organizational? An alternative to
+ * having a service?
+ */
 function getGoogleMaps(apiKey: string): Promise<any> {
   const win = window as any;
   const googleModule = win.google;
