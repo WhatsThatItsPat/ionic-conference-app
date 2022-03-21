359b6845b48b236f4adefa1cd3bc86d3be566d5b
Patrick McDonald <764290+WhatsThatItsPat@users.noreply.github.com>
2022-03-21 10:39:14 -0400

    Replace one that used the array format

--- src/app/providers/check-tutorial.service.ts
+++ src/app/providers/check-tutorial.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@angular/core';
 import { CanLoad, Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 import { Storage } from '@ionic/storage';
 @Injectable({
   providedIn: 'root'
 })
 export class CheckTutorial implements CanLoad {
-  constructor(private storage: Storage, private router: Router) {}
+  constructor(
+    private storage: Storage,
+    private router: Router,
+    private navController: NavController,
+  ) {}
 
   canLoad() {
     return this.storage.get('ion_did_tutorial').then(res => {
       if (res) {
-        this.router.navigate(['/app', 'tabs', 'schedule']);
+        // Here's another.
+        // this.router.navigate(['/app', 'tabs', 'schedule']);
+        this.navController.navigateRoot('/app/tabs/schedule');
         return false;
       } else {
         return true;
